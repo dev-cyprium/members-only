0c10b9895af1bfd768f243cab51a72350eb7f7f8
destroyergm <stefan_vg@hotmail.com>
2016-05-08 21:12:54 +0200

    Add user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 50 }
+	validates :password, presence: true
+	validates :password_confirmation, presence: true
 
 	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	def setup
+		@user = User.new(
+				name: "Example User",
+				email: "user@example.com",
+				password: 'password',
+				password_confirmation: 'password'
+			)
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "Name should be present" do
+		@user.name = "     "
+		assert_not @user.valid?
+	end
+
+	test "Email should be present" do
+		@user.email = "    "
+		assert_not @user.valid?
+	end
+
+	test "Name should not be too loong" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+
+	test "Password should be present ( not blank )" do
+		@user.password = @user.password_confirmation = "     "
+		assert_not @user.valid?
+	end
 end
